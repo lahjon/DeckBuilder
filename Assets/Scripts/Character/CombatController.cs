f034f0218d7b8c9f940ff58f87f8bb5939a89434
KingCube <seth.emanuel.nielsen@gmail.com>
2021-01-21 16:51:19 +0100

    blaaaaa

--- Assets/Scripts/Character/CombatController.cs
+++ Assets/Scripts/Character/CombatController.cs
@@ -50,6 +50,8 @@ public class CombatController : MonoBehaviour
 
     void Start()
     {
+        // DEBUG
+        if (WorldSystem.instance.worldState == WorldState.Combat) ;
         SetUpEncounter();
     }
 
