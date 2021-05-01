33cbbf5cf9a0772259b26e7022c7a98b20eab6cd
Fredrik Gard <fredrik.gard@gmail.com>
2021-05-01 14:15:25 +0200

    effect feedback

--- Assets/EffectDisplay.cs
+++ Assets/EffectDisplay.cs
@@ -3,48 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class EffectDisplay : MonoBehaviour, IToolTipable
 {
     public TMP_Text effectLabel;
     public Image image;
-
-    float durationPopin = 0.2f;
-    public Vector3 popinStartSize = new Vector3(0f, 0f, 0f);
-    public Vector3 fullSize = Vector3.one;
-
-
+    Tween myTween;
     public EffectType backingType;
-
-    IEnumerator popinFunction;
-
-    private void Awake()
-    {
-        popinFunction = Popin();
-    }
 
     private void OnEnable()
     {
-        transform.localScale = popinStartSize;
-        StartCoroutine(popinFunction);
-    }
-
-    public IEnumerator Popin()
-    {
-        float time = 0;
-        while(time < durationPopin)
-        {
-            time += Time.deltaTime;
-            transform.localScale = Vector3.Lerp(popinStartSize, fullSize, time / durationPopin);
-            yield return null;
-        }
-
-        transform.localScale = fullSize;
+        transform.localScale = Vector3.zero;
+        transform.DOScale(1, 0.2f);
     }
 
     public void SetLabel(string stackNr)
     {
-         effectLabel.text = stackNr;
+        effectLabel.text = stackNr;
+        if(myTween != null)
+        {
+            myTween.Kill();
+            myTween = null;
+            effectLabel.transform.localScale = Vector3.one;
+        }
+        myTween = effectLabel.transform.DOScale(1.3f, 0.2f).SetEase(Ease.InOutSine).SetLoops(1, LoopType.Yoyo);
     }
 
     public void SetSprite(Sprite sprite)
